0917b657503277c2a161218b7f969c3863d5d502
Masahiro Suzuka <masahiro.suzuka@gmail.com>
2014-10-04 18:56:18 +0900

    Has enable load CDVViewController

---
+++ platforms/ios/HelloCordova-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Cordova/CDVViewController.h>
+#import <Cordova/CDVCommandDelegateImpl.h>
+#import <Cordova/CDVCommandQueue.h>
--- platforms/ios/HelloCordova/Classes/ViewController.swift
+++ platforms/ios/HelloCordova/Classes/ViewController.swift
@@ -9,24 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  @IBOutlet weak var label: UILabel!
-  
-  @IBAction func print(sender: UIButton) {
-    label.text = "Hello Swift";
-  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    println("HelloSwift");
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
 
+class MainCommandDelegate: CDVCommandDelegateImpl {
+  
+  override func getCommandInstance(pluginName: String!) -> AnyObject! {
+    return getCommandInstance(pluginName);
+  }
+  
+  override func pathForResource(resourcepath: String!) -> String! {
+    return super.pathForResource(resourcepath);
+  }
+}
+
+class MainCommandQueue: CDVCommandQueue {
+  
+  override func execute(command: CDVInvokedUrlCommand!) -> Bool {
+    return super.execute(command);
+  }
+}
